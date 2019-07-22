ac4eb04be2e88481d941e44a151f59ccd4783b5e
Pavel-dp <43936378+Pavel-dp@users.noreply.github.com>
2019-07-22 12:20:46 +0300

    Update index_2.js

--- index_2.js
+++ index_2.js
@@ -6,7 +6,6 @@ let dataFile = 'data.txt';
  
 fs.appendFile(dataFile, ('\n' + addedData), function(error){
     if(error) throw error; 
-    console.log("File writing completed. File contents:");
-    let data = fs.readFileSync("data.txt", "utf8");
-    console.log(data);  // output the read data
+    console.log("File writing completed. File contents:\n");
+    console.log(fs.readFileSync(dataFile, "utf8");  // output the read data
 });
